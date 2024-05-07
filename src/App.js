4e28cc06527e2fd944357c7b5d0ac29ecb09ef0b
D Deryl Downey <133974812+dderyldowney@users.noreply.github.com>
2024-05-06 22:30:31 -0400

    Basic layout and skeleton completed.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to My's Server!
+          Edit this file, located at <code>src/App.js</code>. Save changes to reload.
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
-test('renders learn react link', () => {
+it ('renders without crashing', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(<App div />);
+  root.unmount();
+});
+
+it ('renders "learn react" link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
